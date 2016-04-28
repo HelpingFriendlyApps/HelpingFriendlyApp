10e4e6d52d3aad5a3536578376d045726ad1f17b
Andrew Huang <andrew.huang12@gmail.com>
2016-04-27 18:29:43 -0700

    Improve MapTab formatting
     * Add 'Top 3 Things' placeholder below map

--- src/containers/MapTab.js
+++ src/containers/MapTab.js
@@ -13,7 +13,7 @@ export default class MapTab extends React.Component {
       latitude: data.latitude,
       longitude: data.longitude,
       title: data.venue,
-      subtitle: `Show Time: ${data.showTime}`
+      subtitle: `${data.address}, ${data.city}, ${data.state}`
     }];
 
     return (
@@ -21,22 +21,28 @@ export default class MapTab extends React.Component {
         <Text style={styles.venue}>{data.venue}</Text>
         <View style={styles.basicInfoContainer}>
           <View style={styles.timeContainer}>
-            <Text style={styles.bold}>Show Time: </Text><Text>{data.showTime}</Text>
-            <Text style={styles.bold}>Doors Open: </Text><Text>{data.doorTime}</Text>
-            <Text style={styles.bold}>Capacity: </Text><Text>{data.capacity}</Text>
+            <Text style={styles.bold}>Show Time: </Text>
+            <Text style={styles.bold}>Doors Open: </Text>
           </View>
           <View style={styles.locationContainer}>
-            <Text style={styles.address}>{data.address}</Text>
-            <Text style={styles.address}>{data.city}, {data.state}</Text>
+            <Text style={styles.bold}>{data.showTime}</Text>
+            <Text style={styles.bold}>{data.doorTime}</Text>
           </View>
         </View>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            region={region}
+            annotations={annotations}
+          />
+        </View>
+        <View style={styles.bottomContainer}>
+          <Text style={styles.topThree}>Top 3 Things to Do in {data.city}</Text>
+          <Text>1. Collect underpants</Text>
+          <Text>2. ?</Text>
+          <Text>3. Profit</Text>
 
-        <MapView
-          style={styles.map}
-          region={region}
-          annotations={annotations}
-        >
-        </MapView>
+        </View>
       </View>
     );
   }
@@ -44,36 +50,43 @@ export default class MapTab extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-  },
-  map: {
-    height: 200,
-    width: 300
-  },
-  bold: {
-    fontWeight: '500',
-    flexDirection: 'row'
-  },
-  basicInfoContainer: {
-    flexDirection: 'row',
-    // paddingBottom: 20,
-    // width: 300,
-    justifyContent: 'space-between'
-  },
-  timeContainer: {
-    // alignItems: 'stretch'
-  },
-  locationContainer: {
-    // alignSelf: 'stretch',
-  },
-  address: {
-    textAlign: 'right'
+    // alignItems: 'center',
+    // flexDirection: 'column',
+    padding: 20
   },
   venue: {
-    fontSize: 25,
+    fontSize: 30,
     letterSpacing: 1,
     color: 'darkslategrey',
     fontWeight: '500',
-    textAlign: 'center'
+    paddingBottom: 20
   },
+  bold: {
+    fontWeight: '500',
+    flexDirection: 'row',
+    fontSize: 15,
+    paddingRight: 5,
+    color: 'darkslategrey'
+  },
+  basicInfoContainer: {
+    flexDirection: 'row',
+    paddingBottom: 40,
+  },
+  mapContainer: {
+    alignItems: 'center',
+    paddingBottom: 40
+  },
+  map: {
+    height: 200,
+    width: 340,
+    alignItems: 'center'
+  },
+  bottomContainer: {
+  },
+  topThree: {
+    fontSize: 20,
+    color: 'darkslategrey',
+    fontWeight: '500',
+    paddingBottom: 10
+  }
 });
