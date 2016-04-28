862286ac8786a1e37a048e2952ae404c13dd5993
Andrew Huang <andrew.huang12@gmail.com>
2016-04-28 13:17:17 -0700

    Refactor changeTab action to ES6

--- src/actions/tabBar.js
+++ src/actions/tabBar.js
@@ -2,7 +2,7 @@ export const CHANGE_TAB = 'CHANGE_TAB';
 
 // These functions are our actions, and are accessible by components via this.props.xxx due to mapDispatchToProps
 
-export function changeTab(nextTab) {
+export const changeTab = (nextTab) => {
   return {
     type: CHANGE_TAB,
     nextTab
