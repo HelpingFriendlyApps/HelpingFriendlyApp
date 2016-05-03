f98cddeb56c33eca9848ac3a863b8d26be533a16
Andrew Huang <andrew.huang12@gmail.com>
2016-05-03 16:18:28 -0700

    Replace BackButton arrow with vector icon

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,19 +1,26 @@
-import React, { Text } from 'react-native';
+import React, { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class BackButton extends React.Component {
   render() {
     return (
-      <Text style={styles.back}>
-        {'<'}
-      </Text>
+      <View style={styles.container}>
+        <Icon
+          name='chevron-left'
+          size={20}
+          style={styles.back}
+        />
+      </View>
     );
   }
 }
 
 const styles = {
+  container: {
+    marginLeft: 20,
+    width: 30
+  },
   back: {
-    fontSize: 25,
-    paddingLeft: 10,
     color: 'white',
   }
 };
