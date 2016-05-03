3de342696caef51d2f49a4a8204915f49e5b5d08
Andrew Huang <andrew.huang12@gmail.com>
2016-05-03 15:20:34 -0700

    Clean up BackButton

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -3,7 +3,9 @@ import React, { Text } from 'react-native';
 export default class BackButton extends React.Component {
   render() {
     return (
-      <Text style={styles.back}>&lt; {this.props.previousScreen}</Text>
+      <Text style={styles.back}>
+        {'<'}
+      </Text>
     );
   }
 }
