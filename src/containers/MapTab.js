6da0d5cf4461de1f46846c8ba9d6b12b76900b91
Andrew Huang <andrew.huang12@gmail.com>
2016-05-23 14:19:07 -0700

    Use native Accordion underlayColor prop

--- src/containers/MapTab.js
+++ src/containers/MapTab.js
@@ -61,7 +61,7 @@ export default class MapTab extends React.Component {
               sections={data.thingsToDo}
               renderHeader={this.renderHeader}
               renderContent={this.renderContent}
-              useTouchableOpacity={true}
+              underlayColor={'transparent'}
             />
           </ScrollView>
         </View>
